518109a3629b7e7f0b0661dd39ccea7a9035ecb5
unknown <sal.fiengo@gmail.com>
2021-05-27 01:44:49 +0100

    added info to test first deploy on netifly

---
+++ src/mock-data/data.js
@@ -0,0 +1,91 @@
+export const cardTitles = ['Front-end', 'Back-end', 'Game'];
+export const frontEnd = [{name:'Javascript', value:'85'}, {name:'React',value:'60'}, {name:'CSS3',value:'90'}, {name:'HTML5',value:'100'}, {name:'Bootstrap',value:'80'}];
+export const backEnd = [{name:'Python',value:'85'}, {name:'Django',value:'60'}, {name:'REST api',value:'50'}];
+export const game = [{name:'PyGame',value:'50'}]
+export const personalInfo = [{name:'Salvatore Fiengo',role:'Aspiring Developer',address:'49 Shandon Mill, Shanon Park, Dublin 7',email:'Sal.fiengo@gmail.com',phone:'+353(0)834320505'}]
+export const linksAndEdu = {
+    gitHub:'https://github.com/SalvatoreFiengo/my_resume.git',
+    linkedin:'https://www.linkedin.com/in/salvatorefiengo',
+    college:{ 
+        name:'Code Institute',
+        date:'02/2019',
+        place:'Dublin, Dublin Co.',
+        description:['Diploma in Software Development','Full Stack Web Development, First Class Honours'],
+        certdata:'https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/15065319',
+        imgSrc:'https://media-exp1.licdn.com/dms/image/C510BAQGMQMWodJcvEQ/company-logo_100_100/0/1519903480042?e=1629331200&v=beta&t=nEZPmYweZW-SEpki2g0UylUQolmuGfA3ycsJkRS9ThY',
+    },
+    cert:{
+        name:'MTA Python Certificate',
+        date:'02/2021',
+        place:'Dublin, Dublin Co.',
+        description:['Introduction to Programming Using Python','Skills and knowledge to recognize and write syntactically correct Python code, recognize data types supported by Python and the ability to recognize and write Python code that will logically solve a given problem.'],
+        certdata:`<div data-iframe-width="120" data-iframe-height="250" data-share-badge-id="da6eb3c2-2569-4c79-8881-85e821879690" data-share-badge-host="https://www.credly.com"></div>`,
+
+    }
+}
+export const jobs=[
+    {
+        id:'0',
+        name:'Collections Team Leader',
+        place: 'Accenture, Dublin, Dublin Co.',
+        description:[
+                'Oversaw talented team of 15 Collection agents by actively communicating project information, remedying issues and delivering positive feedback.',
+                'Evaluated employee skills and knowledge regularly.',
+                'Created support documentation that empowered user community to extend skills and find solutions to questions without help from seniors and team leaders.'
+            ]           
+        ,
+        date: '06/2020 - current'
+    },
+    {
+        id:'1',
+        name: 'Senior Credit Collections Analyst',
+        place: 'Accenture, Dublin, Dublin Co.',
+        description: [
+            'Reviewed customer files on regular basis to make sure receivables were in sound condition.',
+            'Made decisions and recommendations about extending lines of credit.',
+            'Performed credit reviews on corporations to assess financial conditions.',
+            'Increased customer satisfaction by resolving billing issues.',
+            'Awarded best collector year 2018',            
+        ],
+        date: '06/2020 - current'
+    },
+    {
+        id:'2',
+        name: 'Senior Credit Collections Analyst',
+        place: 'Accenture, Dublin, Dublin Co.',
+        description: [
+            'Reviewed customer files on regular basis to make sure receivables were in sound condition.',
+            'Made decisions and recommendations about extending lines of credit.',
+            'Performed credit reviews on corporations to assess financial conditions.',
+            'Increased customer satisfaction by resolving billing issues.',
+            'Awarded best collector year 2018',            
+        ],
+        date: '07/2016 - 06/2020'
+    },
+    {
+        id:'3',
+        name: 'Senior Credit Collections Analyst',
+        place: 'Accenture, Dublin, Dublin Co.',
+        description: [
+            'Reviewed customer files on regular basis to make sure receivables were in sound condition.',
+            'Made decisions and recommendations about extending lines of credit.',
+            'Performed credit reviews on corporations to assess financial conditions.',
+            'Increased customer satisfaction by resolving billing issues.',
+            'Awarded best collector year 2018',            
+        ],
+        date: '07/2016 - 06/2020'
+    },
+    {
+        id:'4',
+        name: 'Senior Credit Collections Analyst',
+        place: 'Accenture, Dublin, Dublin Co.',
+        description: [
+            'Reviewed customer files on regular basis to make sure receivables were in sound condition.',
+            'Made decisions and recommendations about extending lines of credit.',
+            'Performed credit reviews on corporations to assess financial conditions.',
+            'Increased customer satisfaction by resolving billing issues.',
+            'Awarded best collector year 2018',            
+        ],
+        date: '07/2016 - 06/2020'
+    }
+]
