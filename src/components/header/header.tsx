075c65b9a4bccd0d4dfca44812b0de2392da7c0e
unknown <sal.fiengo@gmail.com>
2021-06-03 17:48:40 +0100

    navbar component refactored using styled components

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import Navbar from './navbar';
 import Background from '../../images/MaxPixel.net-Rome-Italy-Colosseum-Roman-Coliseum-City-2030643.jpg';
+import styled from 'styled-components';
 
 type ChildProps = {
     cvPosition: number,
@@ -13,9 +14,30 @@ const Header: FC<ChildProps>= ( { cvPosition, skillPosition, workHistoryPos, edu
     return(
         <div className='header mb-5'>
             <Navbar cvPosition={ cvPosition } skillPosition={ skillPosition } workHistoryPos={ workHistoryPos } educationPos={ educationPos }/>
-            <div className='header-img' style={ { backgroundImage: `url(${Background})`} }>
-            </div>
+            <HeaderImg className='header-img' style={ { backgroundImage: `url(${Background})`} }>
+            </HeaderImg>
         </div>
     )
 }
+const HeaderImg = styled.div`
+    height: 415px;
+    background-size: cover;
+    background-color: #242832;
+    background-repeat: no-repeat;
+    background-position: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 1;
+    &:before {
+        content: '';
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        background-color: rgba(44, 51, 64, 0.7);
+    }
+`
 export default Header
--- src/components/header/navbar.tsx
+++ src/components/header/navbar.tsx
@@ -1,4 +1,6 @@
 import { FC, MouseEvent, useState, useEffect } from 'react'
+import { keyframes } from 'styled-components';
+import styled from 'styled-components';
 
 type ChildProps = {
     cvPosition: number,
@@ -56,36 +58,143 @@ const Navbar:FC<ChildProps> = ({ cvPosition, skillPosition,workHistoryPos, educa
     useEffect(handleSpyScroll);
 
     return(
-        <nav className={['w-100 pt-2 animated', navBackground].join(' ')}>
+        <Nav className={['w-100 pt-2', navBackground].join(' ')}>
             <div>
                 <div className='row'>
                     <div className='col-4 col-md-3'>
-                        <button className="anchor navbar-brand ms-3 ms-md-5 pt-0" onClick={()=>setPos(0)}>My Resume</button>
+                        <ButtonLink className="navbar-brand ms-3 ms-md-5 pt-0" onClick={()=>setPos(0)}>My Resume</ButtonLink>
                     </div>
                     <div className='d-block d-md-none col-8 text-end'>
-                        <button className="border mb-1 me-5 bg-transparent" type="button" onClick={(e)=>handleNavButton(e)}>
+                        <button className="border mb-1 me-5" type="button" onClick={(e)=>handleNavButton(e)}>
                             <i className="fa fa-2x fa-bars" aria-hidden="true"></i>
                         </button>
                     </div>
                     <div className='col-4 col-md-7'>
-                        <ul className={['nav-list d-md-inline mr-3 mt-md-2 mb-1',mobileNavDisplay].join(' ')}>
+                        <NavList className={['d-md-inline mr-3 mt-md-2 mb-1',mobileNavDisplay].join(' ')}>
                             <li className="d-md-inline me-md-3">
-                                <button className='anchor' onClick={()=>setPos(1)}>Presentation</button>
+                                <ButtonLink onClick={()=>setPos(1)}>Presentation</ButtonLink>
                             </li>
                             <li className="d-md-inline me-md-3">
-                                <button className='anchor' onClick={()=>setPos(2)}>Skills</button>
+                                <ButtonLink onClick={()=>setPos(2)}>Skills</ButtonLink>
                             </li>
                             <li className="d-md-inline me-md-3">
-                                <button className='anchor' onClick={()=>setPos(3)}>Education</button>
+                                <ButtonLink onClick={()=>setPos(3)}>Education</ButtonLink>
                             </li>
                             <li className="d-md-inline">
-                                <button className='anchor' onClick={()=>setPos(4)}>Work History</button>
+                                <ButtonLink onClick={()=>setPos(4)}>Work History</ButtonLink>
                             </li>
-                        </ul>
+                        </NavList>
                     </div>
                 </div>
             </div>
-        </nav>
+        </Nav>
     )
 };
+
+const showUp = keyframes`
+    0% { opacity: 0; transform: translateY(-20px); }
+    100% { opacity: 1; transform: translateY(0); }
+`
+const ButtonLink = styled.button`
+    border: none;
+    background-color: inherit;
+    width: 100%;
+
+    @media screen and (min-width:768px){
+        width: auto;
+    }
+`
+
+const Nav = styled.nav`
+    opacity: 1;
+    animation: ${showUp} 1.5s normal;
+    &.fixed-top{
+        background-color: #63a184;
+        outline: 1px solid #242832;
+        outline-offset: -1px;
+        button {
+            color: #fff;
+            background-color: transparent;
+            &:hover{
+                background-color: #efefef;
+                color: #242832; 
+            }
+        }
+        & ul {
+            background-color: #63a184;
+            border:1px solid #242832;
+            border-top:0;
+
+        }
+    }
+    &.nav-relative{
+        z-index: 2;
+        position: relative;
+        button {
+            color: #fff;
+            background-color: transparent;
+            &:hover{
+                background-color: #63a184;
+                color: #242832;
+            }
+        }
+        & ul{
+            background-color: #efefef !important;
+            border:1px solid #fff;
+            button {
+                color: #242832; 
+                &:hover{
+                    background-color: #63a184;
+                    color: #fff;
+                }
+            }
+        }
+        
+    }
+    @media screen and (min-width:768px){
+        &.fixed-top{
+            & ul {
+                background-color: #63a184;
+                border:0;
+            }
+        }
+        &.nav-relative{
+            z-index: 2;
+            position: relative;
+                & ul{
+                    background-color: transparent !important;
+                    border:0;
+                    button {
+                        color: #fff;
+                        &:hover {
+                            color: #242832;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+`
+const NavList = styled.ul`
+    list-style: none;
+    padding: 0;
+    position: absolute;
+    right: 1rem;
+    z-index: 2;
+    box-shadow: 0 1px 6px rgb(0 0 0 / 12%), 0 1px 4px rgb(0 0 0 / 24%);
+    border-bottom-left-radius: 4px;
+    border-bottom-right-radius: 4px;
+
+    @media screen and (min-width:768px){
+        position: initial;
+        display: block;
+        float: right;
+        box-shadow: unset;
+        border: 0;
+        border-radius: unset;
+    }
+`
+
+
 export default Navbar;
